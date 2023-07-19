1e09952490192cf4eab7abb54b956d62436eaebf
titadachFF <>
2023-07-19 10:18:42 +0700

    Connect Successfully to DB

--- models/db.js
+++ models/db.js
@@ -1,25 +1,22 @@
-const mysql = require('mysql');
-const dbConfig = require("../config/dbconfig")
+const {Sequelize} = require("sequelize");
+const dbConfig = require("../config/dbconfig");
+//Create squelize instance
+const sequelize = new Sequelize(dbConfig.DB , dbConfig.USER , dbConfig.PASSWORD ,{
+    host:dbConfig.HOST,
+    dialect:"mysql"
+})
 
-//Create a connection to the database server
-const connection =mysql.createConnection({
-host:dbConfig.HOST,
-user:dbConfig.USER,
-password:dbConfig.PASSWORD,
-database:dbConfig.DB
-});
-
-//Open MYSQL Connection
-connection.connect(
-    (error)=>{
-        if(error) throw error;
-        console.log("Successfully connected to the database...");
+//Test the database connection
+async function testConnection() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
     }
-);
+    
+}
 
-module.exports = connection;
-
-
-
-
+testConnection();
+module.exports = sequelize;
 
